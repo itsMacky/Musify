8ae6a0da69ff18f08ff506e185003be792e6c4ec
Macky <makrand586@gmail.com>
2020-08-03 00:56:24 +0530

    Updates

---
+++ precache-manifest.be90bea8871ef7a5e46177faba6b7a9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958b2fb46f39b9dc116dc49f85403c1b",
+    "url": "/musify/index.html"
+  },
+  {
+    "revision": "92ca663325e0f9fac639",
+    "url": "/musify/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "92e4b7402cf8dc03ece3",
+    "url": "/musify/static/js/2.f674c105.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/musify/static/js/2.f674c105.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92ca663325e0f9fac639",
+    "url": "/musify/static/js/main.9d5f95d7.chunk.js"
+  },
+  {
+    "revision": "1fc481d7ff64383e749b",
+    "url": "/musify/static/js/runtime-main.521fd70b.js"
+  }
+]);
